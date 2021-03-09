b857d15b1c3d8f4cfb6286740735c0e597512410
AFTM7512 <jmethods@163.com>
2021-03-09 21:02:50 +0800

    feat: 增加 pre-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import RouterConfig from "router/router-config";
 import RenderRouter from "router/RenderRouter";
-import styles  from "./app.scss";
+import styles from "./app.scss";
 
 function App() {
+  console.log(111);
   return (
-    <div className={ styles.app }>
-      <RenderRouter routes={ RouterConfig }  />
+    <div className={styles.app}>
+      <RenderRouter routes={RouterConfig} />
     </div>
   );
 }
